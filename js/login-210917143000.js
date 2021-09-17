9afad8526c9b21a15ac49679b02752401995d41c
Daniel <daniboygs@hotmail.com>
2021-09-17 14:48:31 -0500

    release 210917145000

---
+++ js/login-210917143000.js
@@ -0,0 +1,87 @@
+        $(document).ready(function(){ 
+
+    /*checkSession({
+        success: {
+            function: redirectTo,
+            attr: {
+                admin: 'src/main/main.php',
+            }
+        },
+        failed: {
+            function: null,
+            attr: null
+        },
+        location: 'service/check_session.php'
+    });*/
+    
+    jQuery('#login-form').submit(login);
+    
+    function login(){
+
+        $.ajax({  
+            type: "POST",  
+            url: "service/login.php", 
+            dataType : 'json', 
+            data: {
+                auth: JSON.stringify({
+                    username: $("#user").val(),
+                    password: $("#pass").val()
+                })
+            },
+        }).done(function(response){
+
+            if(response.state != "fail"){
+
+                if(response.data){
+                    setSessionVariables('user', response.data);
+                    //showLoading(true);
+
+                    switch(response.data.type){
+                        case 1:
+                            redirectTo('src/admin/admin.php');
+                            break;
+                        case 2:
+                            redirectTo('src/main/main.php');
+                            break;
+                        case 4:
+                            redirectTo('src/admin/admin.php');
+                            break;
+                        case 5:
+                            redirectTo('src/admin/admin.php');
+                            break;
+                        default:
+                            redirectTo('src/main/main.php');
+                            break;
+                    }
+                    
+                    
+                    console.log('data: ', data);
+
+                    Swal.fire('Bienvenido', '', 'success');
+                }
+                else{
+                    /*blurt({
+                        'title' : 'Usuario incorrecto', 
+                        'text' : 'Usuario o contraseña incorrecto', 
+                        'type' : 'error'
+                    });*/
+                    //alert("Usuario incorrecto");
+                    Swal.fire('Usuario incorrecto', 'Intentelo de nuevo!', 'warning')
+                }
+            }
+            else{
+                /*blurt({
+                    'title' : 'Falla de conexión', 
+                    'text' : 'Ha fallado la conexión, porfavor intente de nuevo en unos momentos', 
+                    'type' : 'error'
+                });*/
+                Swal.fire('Oops...', 'Ha fallado la conexión!', 'error');
+                //alert("Ha fallado la conexión, porfavor intente de nuevo en unos momentos");
+            }
+			
+		});  
+        
+        return false;
+    }
+	
+});
---
+++ src/main/json/data-210917143000.js
@@ -0,0 +1,13 @@
+var handle_data = {
+	"agreements": {
+		"active": true,
+		"data": [],
+		"loaded_data": false,
+		"loading": false
+	},
+	"current_multiselect": null,
+	"inegi_crimes": null,
+	"rejected_folders_has_changed": false,
+	"rejected_folders_changed_folders": [],
+	"current_records_search_data": null
+}
