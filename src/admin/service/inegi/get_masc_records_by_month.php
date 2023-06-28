a66249ed0e50f9c1e5a5fa4c13fc3b7082c8cb50
Daniel <daniboygs@hotmail.com>
2023-06-28 14:03:54 -0600

    get_records_by_month

--- src/admin/service/inegi/get_masc_records_by_month.php
+++ src/admin/service/inegi/get_masc_records_by_month.php
@@ -6,16 +6,50 @@ include("../common.php");
 $params = array();
 $options = array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
 $conn = $connections['cmasc']['conn'];
-$db_table = '[inegi].[MASC] m INNER JOIN [inegi].[General] g ON m.GeneralID = g.GeneralID INNER JOIN [cat].[TipoReparacion] tr ON m.TipoReparacion = tr.TipoReparacionID
+$db_table = '[inegi].[MASC] m INNER JOIN (
+	SELECT g.[GeneralID]
+		  ,g.[NUC]
+		  ,g.[FechaInicioSigi]
+		  ,g.[Fecha]
+		  ,g.[Atendidos]
+		  ,acu.[CarpetaRecibidaID] 
+		  ,g.[UnidadID]
+		  ,g.[FiscaliaID]
+		  ,g.[UsuarioID] FROM [inegi].[General] g INNER JOIN AcuerdosCelebrados acu ON acu.AcuerdoCelebradoID = g.AcuerdoCelebradoID
+	UNION
+	SELECT [GeneralID]
+		  ,[NUC]
+		  ,[FechaInicioSigi]
+		  ,[Fecha]
+		  ,[Atendidos]
+		  ,[CarpetaRecibidaID] 
+		  ,[UnidadID]
+		  ,[FiscaliaID]
+		  ,[UsuarioID] FROM [inegi].[General] WHERE CarpetaRecibidaID is not null
+	) g ON m.GeneralID = g.GeneralID INNER JOIN [cat].[TipoReparacion] tr ON m.TipoReparacion = tr.TipoReparacionID
 INNER JOIN [cat].[TipoConclusion] tc ON m.TipoConclusion = tc.TipoConclusionID INNER JOIN [cat].[Turnado] t ON m.Turnado = t.TurnadoID INNER JOIN [inegi].[Delito] d ON g.GeneralID = d.GeneralID 
-INNER JOIN cat.Delito cd ON d.DelitoID = cd.DelitoID INNER JOIN cat.Unidad uni on uni.UnidadID = g.UnidadID LEFT JOIN cat.Fiscalia f ON f.FiscaliaID = g.FiscaliaID';
+INNER JOIN cat.Delito cd ON d.DelitoID = cd.DelitoID INNER JOIN cat.Unidad uni on uni.UnidadID = g.UnidadID LEFT JOIN cat.Fiscalia f ON f.FiscaliaID = g.FiscaliaID
+INNER JOIN CarpetasRecibidas cr ON cr.CarpetaRecibidaID = g.CarpetaRecibidaID
+INNER JOIN CarpetasIngresadas ci ON ci.CarpetaIngresadaID = cr.CarpetaIngresadaID';
 
 $initial_date = $_POST['initial_date'];
 $finish_date = $_POST['finish_date'];
 
 $data = (object) array(
+	'general_id' => (object) array(
+		'db_column' => "g.[GeneralID]",
+		'search' => true
+	),
+	'masc_id' => (object) array(
+		'db_column' => 'm.[MASCID]',
+		'search' => true
+	),
 	'nuc' => (object) array(
 		'db_column' => 'g.[NUC]',
+		'search' => true
+	),
+	'entered_date' => (object) array(
+		'db_column' => 'ci.FechaIngreso',
 		'search' => true
 	),
 	'date' => (object) array(
@@ -127,10 +161,17 @@ function getRecord($attr){
 
 			if($date != null)
 				$date = $date->format('d/m/Y');
+
+			$entered_date = $row['FechaIngreso'];
+
+			if($entered_date != null)
+				$entered_date = $entered_date->format('d/m/Y');
 	
 			array_push($return, array(
+				'ID' => $row['GeneralID'],
 				'NUC' => $row['NUC'],
-				'Fecha' => $date,
+				'FechaIngreso' => $entered_date,
+				'FechaCapturaInegi' => $date,
 				'Unidad' => $row['Unidad'],
 				'Delito' => $row['Delito'],
 				'Mecanismo' => $row['Mecanismo'],
